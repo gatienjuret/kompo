11c082f62cf72aee15cd645bb1542f4a0c64fd6f
gatienjuret <gatien.juret@gmail.com>
2025-12-15 02:36:04 +0100

    fix: enforce immediate service worker update and cache cleanup

--- sw.js
+++ sw.js
@@ -1,11 +1,25 @@
 self.addEventListener('install', (e) => {
+  self.skipWaiting(); // Force activation immediately
   e.waitUntil(
-    caches.open('model-app-store-v2').then((cache) => cache.addAll([
+    caches.open('model-app-store-v3').then((cache) => cache.addAll([
       '/',
       '/index.html',
       '/js/app.js',
     ])),
   );
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(keyList.map((key) => {
+        if (key !== 'model-app-store-v3') {
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
+  return self.clients.claim(); // Take control of all clients immediately
 });
 
 self.addEventListener('fetch', (e) => {
